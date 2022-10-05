954914e7aa569a13e73e119be7cf78378623df14
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-10-05 12:48:34 +0300

    test5

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/oxem_studio/" : "/",
+  transpileDependencies: true,
 });
