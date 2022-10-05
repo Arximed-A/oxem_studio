fcf4a18c61a15d3be91be3759e0981ee76d00e61
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-10-05 10:48:15 +0300

    done

---
+++ src/components/initialItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="calc__item">
+    <label for="first_money" class="calc__title"> Первоначальный взнос </label>
+    <div class="calc__wrapper">
+      <input
+        id="first_money"
+        type="text"
+        class="calc__hole"
+        v-model="firstMoney"
+        disabled
+      />
+      <div class="calc__box calc__box_procent">{{ initial }}%</div>
+    </div>
+    <input
+      class="calc__slider slider"
+      type="range"
+      min="10"
+      max="60"
+      step="1"
+      v-model.number="initial"
+      :disabled="dataStatus"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from "vuex";
+export default {
+  name: "initialItem",
+  data() {
+    return {
+      initial: 13,
+    };
+  },
+  watch: {
+    initial: function () {
+      this.setInitial(this.initial);
+    },
+  },
+  methods: {
+    ...mapActions({
+      setInitial: "setInitial",
+    }),
+  },
+  computed: {
+    ...mapState({
+      dataStatus: (state) => state.dataStatus,
+    }),
+    ...mapGetters({
+      firstMoney: "getFirstMoney",
+    }),
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/monthItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="calc__item">
+    <label for="time" class="calc__title">Срок лизинга</label>
+    <div class="calc__wrapper">
+      <input
+        id="time"
+        type="number"
+        class="calc__hole"
+        v-model.number="month"
+        :disabled="dataStatus"
+      />
+      <div class="calc__box calc__box_month">мес.</div>
+    </div>
+    <input
+      class="calc__slider"
+      type="range"
+      min="1"
+      max="60"
+      step="1"
+      v-model.number="month"
+      :disabled="dataStatus"
+    />
+    <!-- здесь не совсем понял как вы хотите что бы отображались данные -->
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+export default {
+  name: "monthItem",
+  data() {
+    return {
+      month: 60,
+    };
+  },
+  watch: {
+    month: function () {
+      this.setMonth(this.month);
+    },
+  },
+  methods: {
+    ...mapActions({
+      setMonth: "setMonth",
+    }),
+  },
+  computed: {
+    ...mapState({
+      dataStatus: (state) => state.dataStatus,
+    }),
+  },
+};
+</script>
+
+<style lang="scss"></style>
